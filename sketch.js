d82f7da01d8ab82e70d054d4287e5ced750b8ae1
hithaishg <gowdahithaish@gmail.com>
2020-11-30 18:00:32 +0530

    database added

--- sketch.js
+++ sketch.js
@@ -1,13 +1,23 @@
 var ball;
+var database , position ;
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    var ballPosition = database.ref("ball/position") ;
+    ballPosition.on("value",readPosition,showError) ;
+
 }
 
 function draw(){
     background("white");
+    if(position!== undefined) {
+
+
+    
     if(keyDown(LEFT_ARROW)){
         changePosition(-1,0);
     }
@@ -20,10 +30,28 @@ function draw(){
     else if(keyDown(DOWN_ARROW)){
         changePosition(0,+1);
     }
+
     drawSprites();
+}
+}
+function readPosition(data) {
+position = data.val() ;
+ball.x = position.x ;
+ball.y = position.y ;
+
+
+}
+function showError() {
+console.log("error in writing to database") ;
+
+
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref("ball/position") .set({
+        "x":position.x + x ,
+    "y":position.y + y 
+    })
+
+    
 }
